7b723c84d84b2b3c9751c5e39d4a89c21bda9086
vivek <vivekjadhav6088@gmail.com>
2024-02-22 14:58:00 +0530

    first commit

---
+++ Flask Deployed App/app.py
@@ -0,0 +1,80 @@
+import os
+from flask import Flask, render_template, request
+from PIL import Image
+import torchvision.transforms.functional as TF
+import CNN
+import numpy as np
+import torch
+import pandas as pd
+import gdown
+
+app = Flask(__name__)
+
+# Define the URL for the model
+model_url = "https://drive.google.com/uc?id=1c_B9O8vkxHm8AFWXrCmDhQNIq93zBiPj"
+# Define the local path for the model
+model_output_path = "plant_disease_model_1_latest.pt"
+
+# Check if the model file exists locally
+if not os.path.exists(model_output_path):
+    # Download the model from the URL
+    gdown.download(model_url, model_output_path, quiet=False)
+
+# Load the model
+model = CNN.CNN(39)
+model.load_state_dict(torch.load(model_output_path))
+model.eval()
+
+# Load disease information
+disease_info = pd.read_csv('D:\Plant-Disease-Using-Image_processing\Flask Deployed App\disease_info.csv', encoding='cp1252')
+supplement_info = pd.read_csv('D:\Plant-Disease-Using-Image_processing\Flask Deployed App\supplement_info.csv', encoding='cp1252')
+
+def prediction(image_path):
+    image = Image.open(image_path)
+    image = image.resize((224, 224))
+    input_data = TF.to_tensor(image)
+    input_data = input_data.view((-1, 3, 224, 224))
+    output = model(input_data)
+    output = output.detach().numpy()
+    index = np.argmax(output)
+    return index
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/index')
+def ai_engine_page():
+    return render_template('index.html')
+
+@app.route('/mobile-device')
+def mobile_device_detected_page():
+    return render_template('mobile-device.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        image = request.files['image']
+        filename = image.filename
+        file_path = os.path.join('E:/archive/test/test', filename)
+        image.save(file_path)
+        pred = prediction(file_path)
+        title = disease_info['disease_name'][pred]
+        description = disease_info['description'][pred]
+        prevent = disease_info['Possible Steps'][pred]
+        image_url = disease_info['image_url'][pred]
+        supplement_name = supplement_info['supplement name'][pred]
+        supplement_image_url = supplement_info['supplement image'][pred]
+        supplement_buy_link = supplement_info['buy link'][pred]
+        return render_template('submit.html', title=title, desc=description, prevent=prevent,
+                               image_url=image_url, pred=pred, sname=supplement_name,
+                               simage=supplement_image_url, buy_link=supplement_buy_link)
+
+@app.route('/market', methods=['GET', 'POST'])
+def market():
+    return render_template('market.html', supplement_image=list(supplement_info['supplement image']),
+                           supplement_name=list(supplement_info['supplement name']),
+                           disease=list(disease_info['disease_name']), buy=list(supplement_info['buy link']))
+
+if __name__ == '__main__':
+    app.run(debug=True)
